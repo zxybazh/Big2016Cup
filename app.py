7ac4fff669495ae18c0e34cb8ec2fbba9d559f48
Xiyou Zhou <zxybazh@qq.com>
2016-04-06 00:09:25 +0800

    updated a few function with bugs fix

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request
-import urllib2, json, re
+import pandas as pd, csv, xgboost as xgb, numpy as np, \
+	unicodedata, math, json, urllib2, re
 
 dEbUg = True
 
 app = Flask(__name__)
 
-def right():
-	global dEbUg
-	# if dEbUg: print 'result', 'yes'
-	return jsonify({'result': 'yes'})
-
-def wrong():
-	global dEbUg
-	# if dEbUg: print 'result', 'no'
-	return jsonify({'result': 'no'})
-
-def error(e):
-	global dEbUg
-	# if dEbUg: print 'error', e
-	return jsonify({'error' : e})
+def right(): return jsonify({'result': 'yes'})
+def wrong(): return jsonify({'result': 'no'})
+def error(e): return jsonify({'error' : e})
 
 def MAG(data):
 	req = urllib2.Request('http://magraph.cloudapp.net/')
@@ -30,11 +20,13 @@ def GetAuthorName(AuthorID):
 	data = {
 		"path": "/author",
 		"author": {
-		"type": "Author",
-		"id": [AuthorID],
-		"select":[ "Name" ]}
+			"type": "Author",
+			"id": [ AuthorID ],
+			"select":[ "Name" ]
+		}
 	}
 	response = MAG(data)
+	if len(response) == 0: return None
 	return response[0][0]['Name']
 
 def GetAuthorAffiliations(AuthorID):
@@ -42,7 +34,7 @@ def GetAuthorAffiliations(AuthorID):
 		"path": "/author/*/affiliation",
 		"author": {
 			"type": "Author",
-			"id": [AuthorID],
+			"id": [ AuthorID ],
         },
 		"affiliation": {
 			"type": "Affiliation",
@@ -60,8 +52,8 @@ def GetPublicationAuthorPairs(PaperID):
 		"path": "/paper/*/author",
 		"paper": {
 			"type": "Paper",
-			"id" : [PaperID],
-			"select": [ "OriginalPaperTitle" ]
+			"id" : [ PaperID ],
+			"select": [ "OriginalPaperTitle", "NormalizedPaperTitle"]
 		},
 		"author": {
 			"type": "Author",
@@ -70,9 +62,37 @@ def GetPublicationAuthorPairs(PaperID):
 	}
 	return MAG(data)
 
+def CheckPublicationAuthor(PaperID, AuthorID):
+	data = {
+		"path": "/paper/*/author",
+		"paper": {
+			"type": "Paper",
+			"id" : [ PaperID ],
+			"select": [ "OriginalPaperTitle", "NormalizedPaperTitle" ]
+		},
+		"author": {
+			"type": "Author",
+			"select": [ "Name" ]
+		}
+	}
+	for w in MAG(data):
+		if w[1]['CellID'] == AuthorID:
+			return True
+	return False
+
+def regcut(rule, s):
+    return " ".join( \
+		filter(lambda x : len(x) > 0, re.compile(rule).split(s.lower())))
+
+def NormalizeString(s):
+    my_unicode = s.decode('unicode-escape')
+    s = unicodedata \
+		.normalize('NFD', my_unicode) \
+		.encode('ascii', 'ignore')
+    return regcut("[\W+_]", regcut("\<[^\>]*\>", s))
+
 def editdistance(s, t):
-	if (abbr(s) or abbr(t)) and s[0] == t[0]:
-		return 0
+	if (isAbbr(s) or isAbbr(t)) and s[0] == t[0]: return 0
 	m = len(s) + 1
 	n = len(t) + 1
 	opt = {}
@@ -81,23 +101,25 @@ def editdistance(s, t):
 	for i in xrange(1, m):
 		for j in xrange(1, n):
 			cost = 0 if s[i-1] == t[j-1] else 1
-			opt[i, j] = min(opt[i, j-1] + 1, opt[i-1, j] + 1, opt[i-1, j-1] + cost)
+			opt[i, j] = min( opt[i, j-1] + 1, \
+				opt[i-1, j] + 1, \
+				opt[i-1, j-1] + cost)
 	return opt[i, j]
 
-def abbr(x):
-	return len(x) == 1
+def isAbbr(x): return len(x) == 1
 
-def VagueMatch(s, t):
-	SplitString = "##############################"
+def VagueNameMatch(s, t):
 	infinity = 1e9
-	a = filter(lambda x:len(x) > 0, re.compile("[\W]").split(s))
-	b = filter(lambda x:len(x) > 0, re.compile("[\W]").split(t))
+
+	a = NormalizeString(s).split(" ")
+	b = NormalizeString(t).split(" ")
+
 	if (len(a) == len(b)):
 		for i in xrange(len(a)):
-			if (abbr(a[i]) or abbr(b[i])) and a[i][0] == b[i][0]:
+			if isAbbr(a[i]) or isAbbr(b[i]):
 				a[i] = a[i][0]
 				b[i] = b[i][0]
-		return editdistance(SplitString.join(a), SplitString.join(b)) < 3
+		return editdistance(" ".join(a), " ".join(b)) <= 1
 	else:
 		if (len(a) > len(b)): a, b = b, a
 		m = len(a) + 1
@@ -107,85 +129,100 @@ def VagueMatch(s, t):
 		for j in xrange(n): opt[0, j] = 0
 		for i in xrange(1, m):
 			for j in xrange(i, n):
-				cost = 0 if editdistance(a[i - 1], b[j - 1]) < 2 else 1
+				cost = min(editdistance(a[i - 1], b[j - 1]), 2)
 				opt[i, j] = infinity
 				for k in xrange(i-1, j):
 					opt[i, j] = min(opt[i, j], opt[i - 1, k] + cost)
 		ans = infinity
-		for t in xrange(m-1, n):
-			ans = min(opt[m-1, t], ans)
-		return ans < 2
+		for t in xrange(m - 1, n):
+			ans = min(opt[m - 1, t], ans)
+		return ans <= 1
 
 def check(AuthorID, PaperID):
 	global dEbUg
-	AuthorName = GetAuthorName(AuthorID)
-	AuthorAffiliations = GetAuthorAffiliations(AuthorID)
 
+	# Check link
+	if (CheckPublicationAuthor(PaperID, AuthorID)):
+		if dEbUg: print "Linked yes!"
+		return True
+	# todo : Add false link check
+
+	# Get Author Name
+	AuthorName = GetAuthorName(AuthorID)
+	if AuthorName == None: return False
+
+	# Get Author Author's Affiliations
+	temp = GetAuthorAffiliations(AuthorID)
+	AuthorAffiliations = []
+	for w in temp:
+		AuthorAffiliations += \
+			[ NormalizeString(t) for t in w.split("|") ]
+
+	# Debug Information
 	if dEbUg:
 		print "AuthorID	:", AuthorID
 		print "AuthorName	:", AuthorName
 		print "AuthorAffiliations :"
 		for AuthorAffiliation in AuthorAffiliations:
 			print AuthorAffiliation
+		print "-" * 30
 
+	# Get all authors of this publication
 	PublicationAuthorPairs = GetPublicationAuthorPairs(PaperID)
-	# print json.dumps(PublicationAuthorPair, indent=4, sort_keys=True)
-	if dEbUg: print "Checking Authors..."
+	# if dEbUg: print json.dumps(PublicationAuthorPair, indent=4, sort_keys=True)
+
+	# if dEbUg: print "Checking Authors..."
 	for PublicationAuthorPair in PublicationAuthorPairs:
+
 		if dEbUg: print "Checking Author", PublicationAuthorPair[1]['Name']
-		if VagueMatch(PublicationAuthorPair[1]['Name'], AuthorName):
+
+		if VagueNameMatch(PublicationAuthorPair[1]['Name'], AuthorName):
 			if dEbUg: print "Name Matched :)"
-			Flag = False
 			if AuthorAffiliations == []:
-				Flag = True
 				print "No Affiliation information >_<"
-			CandidateAuthorAffiliations = GetAuthorAffiliations(PublicationAuthorPair[1]['CellID'])
+				# todo : deeper check with coauthor information and other
+				return True
+
+			temp = GetAuthorAffiliations(PublicationAuthorPair[1]['CellID'])
+			CandidateAuthorAffiliations = []
+			for w in temp:
+				CandidateAuthorAffiliations += \
+					[ NormalizeString(t) for t in w.split("|") ]
+
 			for CandidateAuthorAffiliation in CandidateAuthorAffiliations:
 				for AuthorAffiliation in AuthorAffiliations:
-					if (CandidateAuthorAffiliation == AuthorAffiliation or CandidateAuthorAffiliation.find(AuthorAffiliation) == 0 or AuthorAffiliation.find(CandidateAuthorAffiliation) == 0):
-						if dEbUg: print "Affiliation Matched :)"
-						Flag = True
-						break
-				if (Flag): break
-			if Flag:
-				return True
-			else:
-				if dEbUg:
-					print "Affiliation Mismatched :("
-					# print AuthorAffiliation
-					# print CandidateAuthorAffiliation
+					if (CandidateAuthorAffiliation == AuthorAffiliation or
+						CandidateAuthorAffiliation.find(AuthorAffiliation) >= 0
+						or AuthorAffiliation.find(CandidateAuthorAffiliation) >= 0):
+							if dEbUg: print "Affiliation Matched :)"
+							# todo : deeper check with coauthor information and other
+							return True
+			if dEbUg: print "No Affiliation Matched :("
+		else:
+			if dEbUg: print "Name Mismatched :("
+	# todo : deeper check with coauthor information and other
+	# Otherwise the answer is no
 	return False
 
 @app.route('/big2016/', methods=['POST'])
 def create_task():
 	global dEbUg
-	if not request.json:
-		return error("json format error")
-	else:
-		data = request.json
+
+	if not request.json: return error("json format error")
+	else: data = request.json
 
 	if dEbUg:
-		print "==============================================="
+		print "=" * 30
 		print data
-
-	'''
-	try:
-		temp = json.loads(data)
-	except Exception,e:
-		return error("json format error")
-	'''
+		print "-" * 30
 
 	if ('paper_id' in data and 'author_id' in data):
 		AuthorID = data['author_id']
 		PaperID = data['paper_id']
-		if check(AuthorID, PaperID):
-			if dEbUg: print "==============================================="
-			return right()
-		else:
-			if dEbUg: print "==============================================="
-			return wrong()
+		if check(AuthorID, PaperID): return right()
+		else: return wrong()
 	else:
 		return error("json content error")
 
 if __name__ == '__main__':
-	app.run(host = "0.0.0.0", port = 23201, debug=True)
+	app.run(host = "0.0.0.0", port = 23201, debug=dEbUg)
